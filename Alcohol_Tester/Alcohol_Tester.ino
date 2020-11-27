2b684ea54dbac27acc0b5e9763f018c5c097681d
Ivan Iliev <mr.i.iliev@gmail.com>
2020-11-27 18:04:30 +0200

    last changes

--- Alcohol_Tester/Alcohol_Tester.ino
+++ Alcohol_Tester/Alcohol_Tester.ino
@@ -1,4 +1,3 @@
-
 /******************************
         MADE BY IVAN ILIEV
          FOR National IT Comp
@@ -7,39 +6,133 @@
         GitHub:ivan-iliev
  ******************************/
 
-//Including the Firebase header so we can send data from the MQ3 sensor
-#include "FirebaseESP8266.h"
-//Includding the Wi-Fi header for the node-mcu board ,so that we can conect to Wi-Fi
-#include <ESP8266WiFi.h>
-//Including the header LCD display header ,so we can display the values from the sensor
+
 #include <LiquidCrystal.h>
-//Defining the firebase db and the wi-fi network
+
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include "FirebaseESP8266.h"
+
+
 #define FIREBASE_HOST "nodemcu-915c6.firebaseio.com"
 #define FIREBASE_AUTH "6Q0Y2gTSJswpeuBoJUQK0yOLsPu2Lj3UzcWSl0Dn"
-#define WIFI_SSID "Wi-Fi_IoT"                                          //Wi-Fi Name
-#define WIFI_PASSWORD "qwertyui"                                        //Wi-Fi Pass
+#define WIFI_SSID "Wi-Fi_IoT"                                          
+#define WIFI_PASSWORD "qwertyui"                                        
 
-//setting the analog pin for the sensor , the path for the db and the variable that will hold the value of the sensor
-const int AOUTpin = 13;
-String path = "/Values";
+
+
+IPAddress ip;
+const int AOUTpin = 0;
 int value;
-
-//declaring a variable for the data that we are sending and making a func that will print our data
+int Contrast = 100;
+LiquidCrystal lcd(04, 00, 02, 14, 12, 13);
+float sensor_volt;
+float RS_gas;
+float R0;
+float ratio;
+float BAC;
+int R2 = 2000;
+const int buttonPin = 16;
+int ledState = 0;
+int btn_State = 0;
+float maxPromil = 0;
 FirebaseData firebaseData;
-void printResult(FirebaseData & data);
-//Setting the contrast and the pins for our LCD display
-int Contrast = 10;
-LiquidCrystal lcd(5, 4, 0, 2, 14, 12);
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
+
+String toString(const IPAddress& address) {
+  return String() + address[0] + "." + address[1] + "." + address[2] + "." + address[3];
+}
+
+
+
+void updateDatabase()
+{
+
+
+
+  String ipString = toString(ip);
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+  firebaseData.setBSSLBufferSize(1024, 1024);
+
+  
+  firebaseData.setResponseSize(1024);
+
+  String wifiMacString = WiFi.macAddress();
+  String path = "/" + wifiMacString;
+  timeClient.update();
+  timeClient.setTimeOffset(0);
+
+  String jsonStr = "";
+
+  FirebaseJson json1;
+
+  FirebaseJsonData jsonObj;
+  long timeStamp = timeClient.getEpochTime();
+  json1.set("/Promiles", BAC * 0.0001);
+  json1.set("/IP", ipString);
+  json1.set("/Timestamp", (String)timeStamp);
+
+  Serial.println("------------------------------------");
+  Serial.println("JSON Data");
+  json1.toString(jsonStr, true);
+  Serial.println(jsonStr);
+  Serial.println("------------------------------------");
+
+  Serial.println("------------------------------------");
+  Serial.println("Set JSON test...");
+
+  if (Firebase.push(firebaseData, path, json1))
+  {
+    Serial.println("PASSED");
+    Serial.println("PATH: " + firebaseData.dataPath());
+    Serial.println("TYPE: " + firebaseData.dataType());
+    Serial.print("VALUE: ");
+
+    Serial.println("------------------------------------");
+    Serial.println();
+
+    maxPromil = 0;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Uploaded");
+    lcd.setCursor(0, 1);
+    lcd.print("Result.");
+
+  }
+  else
+  {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+
+    maxPromil = 0;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Connecton");
+    lcd.setCursor(0, 1);
+    lcd.print("Error.");
+
+
+  }
+
+}
+
+
+
+
+
+
 
 void setup() {
-  //Setting the bauds that our serial monitor will print on
-  Serial.begin(19200);
+  timeClient.begin();
+  Serial.begin(9600);
 
-  //Setting the contrast and the number of collumns and rows on  the display
-  analogWrite(16, Contrast);
-  lcd.begin(16, 2);
-
-  //Conecting to the Wi-Fi
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED)
@@ -47,198 +140,81 @@ void setup() {
     Serial.print(".");
     delay(300);
   }
+  ip = WiFi.localIP();
+
+
   Serial.println();
   Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
   Serial.println();
+  Serial.println();
 
-
-  //Setting up the FireBase
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Firebase.reconnectWiFi(true);
-
-  Firebase.setReadTimeout(firebaseData, 1000 * 60);
-  Firebase.setwriteSizeLimit(firebaseData, "tiny");
+  pinMode(buttonPin, INPUT);
+  analogWrite(5, Contrast);
+  lcd.begin(16, 2);
 }
 
-//function for printing the data that we get from the server
+void loop() {
 
-void printResult(FirebaseData &data)
-{
+  timeClient.update();
 
-
-  Serial.println(data.intData());
-
-}
-
-void loop()
-{
-  //Getting the value from the sensor
   value = analogRead(AOUTpin);
+  sensor_volt = (float)value / 1024 * 5.0;
+  RS_gas = ((5.0 * R2) / sensor_volt) - R2;
+  
+  R0 = 45000;
+  ratio = RS_gas / R0; 
+  double x = 0.4 * ratio;
+  BAC = pow(x, -1.431);f
+  float finalValue = BAC * 0.001;
+  ledState = digitalRead(LED_BUILTIN);
+  btn_State = digitalRead(buttonPin);
 
-  //Checking if the value of the sensor is in the borders
-  if (value < 200)
-  {
-    //showing the message and the value on the display
+
+  if (btn_State == HIGH) {
+    lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print(value);
-
-    lcd.setCursor(0, 1);
-    lcd.print("You are sober:) ");
-
-    //delay so we can send only one value to the server
-    delay(9000);
-
-    //Pushing the data to the db and printing passed if we have send the date and failed if we havent
-    if (Firebase.pushInt(firebaseData, path + "/Sober/Value", value))
-    {
-      Serial.println("PASSED");
-      Serial.println("PATH: " + firebaseData.dataPath());
-      Serial.print("PUSH NAME: ");
-      Serial.println(firebaseData.pushName());
-      Serial.println("ETag: " + firebaseData.ETag());
-      Serial.println("------------------------------------");
-      Serial.println();
-    }
-    else
-    {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + firebaseData.errorReason());
-      Serial.println("------------------------------------");
-      Serial.println();
+    lcd.print("Reading Data.");
+    if (finalValue > maxPromil) {
+      maxPromil = finalValue;
     }
 
-
-
-  }
-  if (value >= 200 && value < 280)
-  {
-
-
-    lcd.setCursor(0, 0);
-    lcd.print(value);
-
-    lcd.setCursor(0, 1);
-    lcd.print("Alcohol detected!           ");
-
-
-    delay(9000);
-    if (Firebase.pushInt(firebaseData, path + "/Low/Value", value))
+  } else if (btn_State == LOW && maxPromil != 0) {
+    Serial.println(maxPromil);
+    if (maxPromil <= 0.1 && maxPromil >= 0.01 )
     {
-      Serial.println("PASSED");
-      Serial.println("PATH: " + firebaseData.dataPath());
-      Serial.print("PUSH NAME: ");
-      Serial.println(firebaseData.pushName());
-      Serial.println("ETag: " + firebaseData.ETag());
-      Serial.println("------------------------------------");
-      Serial.println();
+
+      
+      lcd.setCursor(0, 0);
+      lcd.print(maxPromil);
+      lcd.print(" g/DL");
+
+      lcd.setCursor(0, 1);
+      lcd.print("Low Level of promiles.");
     }
-    else
+    if (maxPromil >= 0.1 && maxPromil <= 0.3)
     {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + firebaseData.errorReason());
-      Serial.println("------------------------------------");
-      Serial.println();
+
+
+      lcd.setCursor(0, 0);
+      lcd.print(maxPromil);
+      lcd.print(" g/DL");
+
+      lcd.setCursor(0, 1);
+      lcd.print("Medium Level of promiles.");
     }
+    if (maxPromil > 0.3) {
 
+      lcd.setCursor(0, 0);
+      lcd.print(maxPromil);
+      lcd.print(" g/DL");
 
-  }
-  if (value >= 280 && value < 350)
-  {
-
-    lcd.setCursor(0, 0);
-    lcd.print(value);
-
-    lcd.setCursor(0, 1);
-    lcd.print("Medium level                      ");
-
-    delay(9000);
-    if (Firebase.pushInt(firebaseData, path + "/Medium/Value", value))
-    {
-      Serial.println("PASSED");
-      Serial.println("PATH: " + firebaseData.dataPath());
-      Serial.print("PUSH NAME: ");
-      Serial.println(firebaseData.pushName());
-      Serial.println("ETag: " + firebaseData.ETag());
-      Serial.println("------------------------------------");
-      Serial.println();
-    }
-    else
-    {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + firebaseData.errorReason());
-      Serial.println("------------------------------------");
-      Serial.println();
+      lcd.setCursor(0, 1);
+      lcd.print("High Level of promiles.");
     }
 
-  }
-  if (value >= 350 && value < 450)
-  {
-    lcd.setCursor(0, 0);
-    lcd.print(value);
-
-    lcd.setCursor(0, 1);
-    lcd.print("Serious Booze!    ");
-
-
-    delay(9000);
-    if (Firebase.pushInt(firebaseData, path + "/High/Value", value))
-    {
-      Serial.println("PASSED");
-      Serial.println("PATH: " + firebaseData.dataPath());
-      Serial.print("PUSH NAME: ");
-      Serial.println(firebaseData.pushName());
-      Serial.println("ETag: " + firebaseData.ETag());
-      Serial.println("------------------------------------");
-      Serial.println();
-    }
-    else
-    {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + firebaseData.errorReason());
-      Serial.println("------------------------------------");
-      Serial.println();
-    }
-
-  }
-  if (value > 450)
-  {
-
-    lcd.setCursor(0, 0);
-    lcd.print(value);
-
-    lcd.setCursor(0, 1);
-    lcd.print("You are drunk!       ");
-
-    delay(9000);
-    if (Firebase.pushInt(firebaseData, path + "/Drunk/Value", value))
-    {
-      Serial.println("PASSED");
-      Serial.println("PATH: " + firebaseData.dataPath());
-      Serial.print("PUSH NAME: ");
-      Serial.println(firebaseData.pushName());
-      Serial.println("ETag: " + firebaseData.ETag());
-      Serial.println("------------------------------------");
-      Serial.println();
-    }
-    else
-    {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + firebaseData.errorReason());
-      Serial.println("------------------------------------");
-      Serial.println();
-    }
-
-    lcd.setCursor(0, 0);
-    lcd.print(value);
-
-    lcd.setCursor(0, 1);
-    lcd.print("You are drunk!");
-
-
-    Serial.println("You are drunk!");
+    updateDatabase();
   }
 
-  //putting delay of 500 miliseconds so that there is a space between each data that we send to the db
-  delay (500);
+
 }
